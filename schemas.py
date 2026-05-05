e5bea189ed4ec4387f4e05b7bd5462d7c9acb475
BrendoMDL <nerian.brendow14ag@gmail.com>
2026-05-04 22:05:28 -0300

    POST/residuos

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class ResiduoCreate(BaseModel):
+    nome: str
+    codigo_barras: str
+    quantidade: int
+
+class ResiduoResponse(BaseModel):
+    id: int
+    nome: str
+    codigo_barras: str
+    quantidade: int
+
+    class Config:
+        from_attributes = True
