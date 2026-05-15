39506f0f104577c28506319af9a6bb0b64e6ef24
Joao Gui <joaoguilhermepinheirotavares@gmail.com>
2026-05-15 19:26:45 -0300

    adicionei not null e minim aos usuario model e alembic

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -4,13 +4,15 @@ Schemas de Usuário
 Define os modelos Pydantic para criação e manipulação de usuários.
 """
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, EmailStr, Field
 
 class UsuarioCreate(BaseModel):
-    """Modelo para criação de um novo usuário."""
+    """Modelo para criação de um novo usuário.
+        Field para criar tamanho mínimo
+        Falta criar um sistema que peça para o usuário digitar o mínimo na hora do erro
+    """
 
-    nome: str
-    email: str
-    telefone: str
-    senha: str
+    nome: str = Field(min_length=1)
+    email: EmailStr
+    telefone: str = Field(min_length=1)
+    senha: str = Field(min_length=6)
